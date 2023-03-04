048ecb6228b7abfba2e40e24f4a60223b26ae3bf
Victor <vitya999123@gmail.com>
2023-03-04 17:19:17 +0200

    changing the style of the total price

--- js/toggleCartStatus.js
+++ js/toggleCartStatus.js
@@ -4,15 +4,17 @@ function toggleCartStatus() {
     const cartWrapper = document.querySelector(".cart-wrapper");
     const cartEmptyBadge = document.querySelector("[data-cart-empty]");
     const orderForm = document.querySelector("#order-form");
-
+    const cartTotal =  document.querySelector(".cart-total");
 
     if (cartWrapper.children.length > 0) {
         cartEmptyBadge.style.display = "none";
         orderForm.style.display = "block";
+        cartTotal.style.display = "block";
 
     } else {
         cartEmptyBadge.style.display = "block";
         orderForm.style.display = "none";
+        cartTotal.style.display = "none";
     }
 
     saveCartTolocalStorage();
